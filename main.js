9fabf6a9d40cef7fca55a04da0d374b56167590f
umarNurmuxamedov2013 <nurmuxamedovumar@gmail.com>
2024-03-16 12:56:31 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let string = String(prompt("String typedagi ma'lumot kiriting !"));
+let number = Number(prompt("Number typedagi ma'lumot kiriting !"))
+let boolean = Boolean(prompt("Boolean typedagi ma'lumot kiriting !"))
+
+alert("Hamma ma'lumotlarni tog'ri kiritganingizga ishinchingiz komilmi ?")
+alert("Siz kiritgan " + string + " so'z String typega tegishli" + "\n" + "Siz kiritgan " + number + " so'zi Number typega tegishili" + "\n" + "Siz kiritgan " + boolean + " so'zi Boolean typega tegishil")
